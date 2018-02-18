0883cdc5aad47f3ada888352a5f4d88558d61931
Ahmed Mostafa <31903930+ahmedmostafa0x61@users.noreply.github.com>
2018-02-18 18:30:27 +0200

    Update Wall-E.py
    Some Changes

--- Wall-E.py
+++ Wall-E.py
@@ -1,71 +1,80 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-
+import numpy as np
+from math import *
 def Draw ():
     #Body
-    glColor(0.7,0,0)
+    glColor(0,0.4,0.8)
     glBegin(GL_POLYGON)
-    glVertex2d(0.6,0.1)
-    glVertex2d(0.6,-0.5)
-    glVertex2d(-0.6,-0.5)
-    glVertex2d(-0.6,0.1)
+    glVertex2d(0.5,0.1)
+    glVertex2d(0.5,-0.5)
+    glVertex2d(-0.5,-0.5)
+    glVertex2d(-0.5,0.1)
     glEnd()
-
+    
+    #Head
+    glBegin(GL_POLYGON)
+    glVertex2d(0.4, 0.35)
+    glVertex2d(0.4, 0.9)
+    glVertex2d(-0.4, 0.9)
+    glVertex2d(-0.4, 0.35)
+    glEnd()
+    
+    #RightHand
+    glColor(0.82, 0.82, 0.82)
+    glBegin(GL_POLYGON)
+    glVertex2d(0.5,0.0)
+    glVertex2d(0.6,0.0)
+    glVertex2d(0.6,-0.15)
+    glVertex2d(0.5,-0.15)
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2d(0.6,-0.15)
+    glVertex2d(0.7,-0.15)
+    glVertex2d(0.7,0.4)
+    glVertex2d(0.6,0.4)
+    glEnd()
+    
+    #LeftHand
+    glColor(0.82, 0.82, 0.82)
+    glBegin(GL_POLYGON)
+    glVertex2d(-0.5, 0.0)
+    glVertex2d(-0.6, 0.0)
+    glVertex2d(-0.6, -0.15)
+    glVertex2d(-0.5, -0.15)
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2d(-0.6, -0.15)
+    glVertex2d(-0.7, -0.15)
+    glVertex2d(-0.7, 0.4)
+    glVertex2d(-0.6, 0.4)
+    glEnd()
+    
     #RightLeg
-    glColor(1, 0.8, 0)
     glBegin(GL_POLYGON)
     glVertex2d(0.2, -0.5)
     glVertex2d(0.2, -0.8)
-    glVertex2d(0.5, -0.8)
-    glVertex2d(0.5, -0.5)
+    glVertex2d(0.36, -0.8)
+    glVertex2d(0.36, -0.5)
     glEnd()
-
+    
     #LeftLeg
-    glColor(1, 0.8, 0)
     glBegin(GL_POLYGON)
     glVertex2d(-0.2, -0.5)
     glVertex2d(-0.2, -0.8)
-    glVertex2d(-0.5, -0.8)
-    glVertex2d(-0.5, -0.5)
+    glVertex2d(-0.36, -0.8)
+    glVertex2d(-0.36, -0.5)
     glEnd()
 
     #Neck
-    glColor(0.6, 0.4, 0.3)
     glBegin(GL_POLYGON)
     glVertex2d(0.13, 0.1)
     glVertex2d(0.13, 0.35)
     glVertex2d(-0.13, 0.35)
     glVertex2d(-0.13,0.1)
     glEnd()
-
-    #Head
-    glColor(0.5, 0.5, 0.5)
-    glBegin(GL_POLYGON)
-    glVertex2d(0.44, 0.35)
-    glVertex2d(0.44, 0.9)
-    glVertex2d(-0.44, 0.9)
-    glVertex2d(-0.44, 0.35)
-    glEnd()
-
-    # RightEye
-    glColor(1, 0, 0)
-    glBegin(GL_POLYGON)
-    glVertex2d(0.15, 0.7)
-    glVertex2d(0.15, 0.8)
-    glVertex2d(0.3, 0.8)
-    glVertex2d(0.3, 0.7)
-    glEnd()
-
-    # LeftEye
-    glColor(1, 0, 0)
-    glBegin(GL_POLYGON)
-    glVertex2d(-0.15, 0.7)
-    glVertex2d(-0.15, 0.8)
-    glVertex2d(-0.3, 0.8)
-    glVertex2d(-0.3, 0.7)
-    glEnd()
-
+    
     #Mouth
     glColor(0.4, 0.6, 0.6)
     glBegin(GL_POLYGON)
@@ -107,12 +116,67 @@ def Draw ():
     glVertex2d(-0.08, 0.5)
     glVertex2d(-0.06, 0.45)
     glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2d(-0.06, 0.45)
+    glVertex2d(-0.04, 0.5)
+    glVertex2d(-0.02, 0.45)
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2d(-0.02, 0.45)
+    glVertex2d(0.0, 0.5)
+    glVertex2d(0.02, 0.45)
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex2d(0.02, 0.45)
+    glVertex2d(0.04, 0.5)
+    glVertex2d(0.06, 0.45)
+    glEnd()
 
+    #RightEye
+    glColor(1, 0, 0)
+    glBegin(GL_LINES)
+    for theta in np.arange(0, 2 * np.pi, 0.01):
+        x = (0.225+0.07 * cos(theta))
+        y = (.75+0.07 * sin(theta))
+        glVertex2d(0.225, 0.75)
+        glVertex2d(x, y)
+    glEnd()
+    
+    # LeftEye
+    glBegin(GL_LINES)
+    for theta in np.arange(0, 2 * np.pi, 0.01):
+        x = (0.225 + 0.07 * cos(theta))
+        y = (.75 + 0.07 * sin(theta))
+        glVertex2d(-0.225, 0.75)
+        glVertex2d(-x, y)
+    glEnd()
+
+
+    #------Bebyon---------------
+    glBegin(GL_LINES)
+    for theta in np.arange(0, pi, 0.001):
+        x = 0.055 * cos(theta)
+        y = 0.055 * sin(theta)
+        glVertex2d(0,0.05)
+        glVertex2d(x, y)
+    glEnd()
+    
+    #---------Buttons--------------
+    glBegin(GL_LINES)
+    for i in np.arange(0.055, -0.4, -0.05):
+        for theta in np.arange(0, 2 * pi, 0.001):
+            x = 0.01 * cos(theta)
+            y = (i + 0.01 * sin(theta))
+            glVertex2d(0, i)
+            glVertex2d(x, y)
+
+    glEnd()
     glFlush()
-#---------- Program -------------------------------------------
+
+#---Main_Program---------
 glutInit()
 glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
-glutInitWindowSize(600,600)
-glutCreateWindow(b"Report")
+glutInitWindowSize(400,400)
+glutCreateWindow(b"Evil Wall-E")
 glutDisplayFunc(Draw)
 glutMainLoop()
